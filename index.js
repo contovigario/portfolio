6aaebb42f9decb56df80b0191b1e1c3da5d1ed94
contovigario <sara.saedades@gmail.com>
2022-01-25 18:09:04 +0000

    Config #7

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const routesHandler = require('./routes/handler.js');
 const cors = require('cors');
 
 const app = express();
-app.use(cors);
+app.use(cors());
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 app.use('/', routesHandler);
