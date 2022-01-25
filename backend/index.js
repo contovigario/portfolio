2798317f13e17b167d59e2b68f125c6495c4a5cf
contovigario <sara.saedades@gmail.com>
2022-01-25 12:21:41 +0000

    Heroku configurations

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 app.use('/', routesHandler);
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
