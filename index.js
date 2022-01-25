4a9ace0f20955fb586ab2590e879d380e4a4ff17
contovigario <sara.saedades@gmail.com>
2022-01-25 17:31:28 +0000

    Add cors

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const routesHandler = require('./routes/handler.js');
+const cors = require('cors');
 
 const app = express();
+app.use(cors);
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 app.use('/', routesHandler);
